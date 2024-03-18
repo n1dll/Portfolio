4958d28f60194bfdfa5fe9a07e9b168c726597d7
n1dll <ngeorgiev89@outlook.com>
2024-03-18 10:09:56 +0200

    ..

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-console.log(resend);
+console.log(process.env.RESEND_API_KEY);
 // const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
