f84d7ea487d9f5a5a07bdd158614e8d81acc1e39
n1dll <ngeorgiev89@outlook.com>
2024-05-15 11:52:31 +0300

    edit

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import TechStack from './components/TechStack'
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col">
       <Navbar />
