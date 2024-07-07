d8bb1f7321114e69deabb7af723ae681e6cafc4c
n1dll <ngeorgiev89@outlook.com>
2024-07-07 18:54:23 +0300

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Overpass_Mono } from 'next/font/google'
 import { ThemeProvider } from "./components/ThemeProvider"
-import Head from 'next/head'
+import Script from 'next/script'
 
 const overpassMono = Overpass_Mono({
   subsets: ['latin'],
@@ -17,16 +17,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <Head>
-        <script
-          id="CookieDeclaration"
-          src="https://consent.cookiebot.com/e0ad604c-3f39-4ad9-9e8b-4a6f9c59d482/cd.js"
-          type="text/javascript"
-          data-cookieconsent="ignore"
-          async
-        >
-        </script>
-      </Head>
+      <Script
+        id="CookieDeclaration"
+        src="https://consent.cookiebot.com/e0ad604c-3f39-4ad9-9e8b-4a6f9c59d482/cd.js"
+        type="text/javascript"
+        // data-cookieconsent="ignore"
+        strategy="beforeInteractive"
+        async
+      >
+      </Script>
 
       <body className={overpassMono.className}>
         <ThemeProvider
