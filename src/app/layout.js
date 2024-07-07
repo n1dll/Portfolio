f99e0bccd07caa3862e6d93225013c90c9b599fb
n1dll <ngeorgiev89@outlook.com>
2024-07-07 17:55:27 +0300

    cookiebot

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
       <Head>
         <script async id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="e0ad604c-3f39-4ad9-9e8b-4a6f9c59d482" data-blockingmode="auto" type="text/javascript"></script>
       </Head>
