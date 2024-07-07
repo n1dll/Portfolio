5c336055b346cdf4c111fdbd1c72de088bc8ba7f
n1dll <ngeorgiev89@outlook.com>
2024-07-07 17:51:51 +0300

    add cookiebot tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Overpass_Mono } from 'next/font/google'
 import { ThemeProvider } from "./components/ThemeProvider"
+import Head from 'next/head'
 
 const overpassMono = Overpass_Mono({
   subsets: ['latin'],
@@ -15,6 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <script async id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="e0ad604c-3f39-4ad9-9e8b-4a6f9c59d482" data-blockingmode="auto" type="text/javascript"></script>
+      </Head>
+
       <body className={overpassMono.className}>
         <ThemeProvider
           attribute="class"
